a3ddeb7f15a11338bd0cbefb75ffd630d5c503c1
DiffeR777 <dmitrii.mail@inbox.ru>
2015-01-28 22:38:04 +0500

    Update gallery plurals

--- app/src/main/java/dv/dv_translate/MiuiGalleryPAK.java
+++ app/src/main/java/dv/dv_translate/MiuiGalleryPAK.java
@@ -21,6 +21,11 @@ public class MiuiGalleryPAK extends SampleAPK{
             this.TryReplacePlural("confirm_delete_has_cloud_local_video", R.plurals.confirm_delete_has_cloud_local_video);
             this.TryReplacePlural("number_of_items_selected", R.plurals.number_of_items_selected);
             this.TryReplacePlural("number_of_pictures", R.plurals.number_of_pictures);
+            this.TryReplacePlural("how_many_pics", R.plurals.how_many_pics);
+            this.TryReplacePlural("how_many_videos", R.plurals.how_many_videos);
+            this.TryReplacePlural("how_many_sub_folders", R.plurals.how_many_sub_folders);
+            this.TryReplacePlural("message_no_delete_access_with_count", R.plurals.message_no_delete_access_with_count);
+            this.TryReplacePlural("pick_with_limited_upper_bound_title", R.plurals.pick_with_limited_upper_bound_title);
             this.TryReplacePlural("number_of_images", R.plurals.number_of_images);
             this.TryReplacePlural("number_of_items", R.plurals.number_of_items);
             LogPluralsDone();
